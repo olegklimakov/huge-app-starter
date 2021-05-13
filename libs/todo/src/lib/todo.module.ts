5ebf511ad61d335b04c0edeeb3955d92f1fe5e40
Oleg <klimakovoleg@yandex.ru>
2021-05-13 21:07:17 +0200

    todo app files

---
+++ libs/todo/src/lib/todo.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { TodoComponent } from './todo.component';
+
+
+
+@NgModule({
+  declarations: [TodoComponent],
+  imports: [
+  ],
+  exports: [TodoComponent]
+})
+export class TodoModule { }
